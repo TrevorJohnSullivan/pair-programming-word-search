6a956c509a95a742f6ec53846eec4b0cef3f7380
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-14 20:22:08 -0330

    update

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,48 @@
+// const wordSearch = (letters, word) => { 
+//   const horizontalJoin = letters.map(ls => ls.join(''));
+//   for (let l of horizontalJoin) {
+//       if (l.includes(word)) return true;
+//   }
+//   return false; 
+// };
+
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+  const numRows = letters.length;
+  const numCols = letters[0].length;
+
+  // Check horizontally
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  // Check vertically
+  for (let col = 0; col < numCols; col++) {
+    let verticalString = '';
+    for (let row = 0; row < numRows; row++) {
+      verticalString += letters[row][col];
     }
-}
+    if (verticalString.includes(word)) {
+      return true;
+    }
+  }
+  
+  return false; 
+};
+
+
+
+
 
 module.exports = wordSearch
+
+
+
+
+// const wordSearch = (letters, word) => { 
+//   const horizontalJoin = letters.map(ls => ls.join(''))
+//   for (l of horizontalJoin) {
+//       if (l.includes(word)) return true
+//   }
+// }
